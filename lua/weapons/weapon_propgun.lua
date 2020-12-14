769337247bcbd2aed686fd374864c57b9689ca1c
B1uDe4D <b1uDe4d@pm.me>
2020-12-14 11:06:42 -0500

    commented 12/14

--- lua/weapons/weapon_propgun.lua
+++ lua/weapons/weapon_propgun.lua
@@ -3,7 +3,7 @@ SWEP.Author			= "B1uDe4D" -- These two options will be shown when you have the w
 SWEP.Instructions		= "Right mouse to pick the prop, Left mouse to fire the prop!"
 
 SWEP.Spawnable = true 
-SWEP.AdminOnly = true
+SWEP.AdminOnly = false
 
 SWEP.Primary.Clipsize = -1
 SWEP.Primary.DefaultClip = -1
@@ -29,63 +29,87 @@ SWEP.WorldModel = "models/weapons/w_pistol.mdl"
 
 SWEP.ShootSound = Sound("Metal.SawbladeStick")
 
+-- Called when the left mouse button is pressed
 function SWEP:PrimaryAttack()
-    
-    self:SetNextPrimaryFire( CurTime() + 0.5)
+	-- This weapon is 'automatic'. This function call below defines
+	-- the rate of fire. Here we set it to shoot every 0.5 seconds.
+	self:SetNextPrimaryFire( CurTime() + 0.5 )	
 
-    self:ThrowProp("models/props/cs_office/Chair_office.mdl")    
+	-- Call 'ThrowChair' on self with this model
+	self:ThrowChair( "models/props/cs_office/Chair_office.mdl" )
 end
+ 
 
+-- Called when the rightmouse button is pressed
 function SWEP:SecondaryAttack()
-    self:SetNextSecondaryFire( CurTime() + 0.1)
+	-- Though the secondary fire isn't automatic
+	-- players shouldn't be able to fire too fast
+	self:SetNextSecondaryFire( CurTime() + 0.1 )
 
-    self:ThrowProp("models/props/cs_office/Chair_office.mdl")
+	self:ThrowChair( "models/props_c17/FurnitureChair001a.mdl" )
 end
 
-function SWEP:ThrowProp(model_file)
-    local owner = self:GetOwner()
+-- A custom function we added. When you call this the player will fire a chair!
+function SWEP:ThrowChair( model_file )
+	local owner = self:GetOwner()
 
-    if ( not owner:IsValid() ) then return end
+	-- Make sure the weapon is being held before trying to throw a chair
+	if ( not owner:IsValid() ) then return end
 
-    self:EmitSound(self.shootSound)
+	-- Play the shoot sound we precached earlier!
+	self:EmitSound( self.ShootSound )
+ 
+	-- If we're the client then this is as much as we want to do.
+	-- We play the sound above on the client due to prediction.
+	-- ( if we didn't they would feel a ping delay during multiplayer )
+	if ( CLIENT ) then return end
 
+	-- Create a prop_physics entity
+	local ent = ents.Create( "prop_physics" )
 
+	-- Always make sure that created entities are actually created!
+	if ( not ent:IsValid() ) then return end
 
-    if ( CLIENT ) then return end
+	-- Set the entity's model to the passed in model
+	ent:SetModel( model_file )
 
+	-- This is the same as owner:EyePos() + (self.Owner:GetAimVector() * 16)
+	-- but the vector methods prevent duplicitous objects from being created
+	-- which is faster and more memory efficient
+	-- AimVector is not directly modified as it is used again later in the function
+	local aimvec = owner:GetAimVector()
+	local pos = aimvec * 16 -- This creates a new vector object
+	pos:Add( owner:EyePos() ) -- This translates the local aimvector to world coordinates
 
-    local  ent = ents.Create("prop_physics")
+	-- Set the position to the player's eye position plus 16 units forward.
+	ent:SetPos( pos )
 
-    if (not ent:IsValid ) then return end
-
-    ent:SetModel( model_file )
-
-
-    local aimvec = owner:GetAimVector()
-    local pos = aimvec *16
-    pos:Add( owner:EyePos )
-
-    ent:SetPos( pos )
-
-    ent:SetAngles( owner:EyeAngles() )
-    ent:Spawn()
-
-
-    local phys = ent:GetPhysicsObject()
-    if( not phys:IsValid() ) then ent:Remove() return end
-
-
-
-    aimvec:Mul( 100 )
-    aimvec:Add( VectorRand( -10, 10 ) )
-    phys:ApplyForceCenter( aimvec )
-
-
-    cleanup.Add(owner, "props", ent)
-
-    undo.Create("Thrown_chair")
-        undo.AddEntity( ent )
-        undo.SetPlayer( owner )
-    undo.Finish()
+	-- Set the angles to the player'e eye angles. Then spawn it.
+	ent:SetAngles( owner:EyeAngles() )
+	ent:Spawn()
+ 
+	-- Now get the physics object. Whenever we get a physics object
+	-- we need to test to make sure its valid before using it.
+	-- If it isn't then we'll remove the entity.
+	local phys = ent:GetPhysicsObject()
+	if ( not phys:IsValid() ) then ent:Remove() return end
+ 
+	-- Now we apply the force - so the chair actually throws instead 
+	-- of just falling to the ground. You can play with this value here
+	-- to adjust how fast we throw it.
+	-- Now that this is the last use of the aimvector vector we created,
+	-- we can directly modify it instead of creating another copy
+	aimvec:Mul( 100 )
+	aimvec:Add( VectorRand( -10, 10 ) ) -- Add a random vector with elements [-10, 10)
+	phys:ApplyForceCenter( aimvec )
+ 
+	-- Assuming we're playing in Sandbox mode we want to add this
+	-- entity to the cleanup and undo lists. This is done like so.
+	cleanup.Add( owner, "props", ent )
+ 
+	undo.Create( "Thrown_Chair" )
+		undo.AddEntity( ent )
+		undo.SetPlayer( owner )
+	undo.Finish()
 end
 
